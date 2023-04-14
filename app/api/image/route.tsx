a880456111b892c166c8d330088b9d7e2ff04772
neokry <neokry.eth@gmail.com>
2023-04-14 12:56:42 +0800

    Add edge

--- app/api/image/route.tsx
+++ app/api/image/route.tsx
@@ -1,6 +1,8 @@
 import { ContractMetadata, Vote } from "@/data/eth";
 import { ImageResponse } from "next/server";
 
+export const runtime = "edge";
+
 export interface ImageGenerationRequest {
   vote: Vote;
   contractMetadata: ContractMetadata;
